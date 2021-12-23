bd3b05a39cf17f086c12632a4196a1e08be82a1c
杜劲松 <farben_dujinsong@farben.com>
2021-12-23 19:37:25 +0800

    init

--- base-common/src/main/java/cn/imadc/application/base/common/context/ReqCtxConstant.java
+++ base-common/src/main/java/cn/imadc/application/base/common/context/ReqCtxConstant.java
@@ -2,4 +2,6 @@ package cn.imadc.application.base.common.context;
 
 public class ReqCtxConstant {
     public static final String TOKEN = "token";
+    public static final String ID = "id";
+    public static final String USER = "user";
 }
