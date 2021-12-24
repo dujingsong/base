178e05910a763bff8e15acc5f4063132bd32a8a1
杜劲松 <1396787304@qq.com>
2021-12-25 02:18:28 +0800

    init

--- base-common/src/main/java/cn/imadc/application/base/common/context/ReqCtxConstant.java
+++ base-common/src/main/java/cn/imadc/application/base/common/context/ReqCtxConstant.java
@@ -4,4 +4,5 @@ public class ReqCtxConstant {
     public static final String TOKEN = "token";
     public static final String ID = "id";
     public static final String USER = "user";
+    public static final String API = "api";
 }
