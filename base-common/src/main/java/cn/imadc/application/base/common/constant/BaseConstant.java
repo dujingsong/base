4966af18a9da5536ad233c0a578bba3c69173f53
杜劲松 <farben_dujinsong@farben.com>
2022-01-29 15:56:54 +0800

    实体基类

---
+++ base-common/src/main/java/cn/imadc/application/base/common/constant/BaseConstant.java
@@ -0,0 +1,41 @@
+package cn.imadc.application.base.common.constant;
+
+/**
+ * <p>
+ * 系统基础常量
+ * </p>
+ *
+ * @author 杜劲松
+ * @since 2021-12-24
+ */
+public class BaseConstant {
+
+    // --------------------基础常量--------------------
+    public static final String COMMA = ",";
+    public static final String BLANK = "";
+    public static final String SLASH = "/";
+
+    // --------------------日期常量--------------------
+    public static final String YYYY_MM_DD = "YYYY-MM-dd";
+
+    // --------------------数据列标识-------------------
+    public final static String CREATE_BY = "create_by";
+    public final static String UPDATE_BY = "update_by";
+
+    public final static String CREATE_BY_ID = "create_by_id";
+    public final static String UPDATE_BY_ID = "update_by_id";
+
+    // --------------------删除标识--------------------
+    public static final String DEL_FLAG = "del_flag";
+    public static final int DEL_VAL = 1;
+    public static final int NOT_DEL_VAL = 0;
+
+    // --------------------认证授权--------------------
+    public static final String ACCESS_TOKEN = "Access-Token";
+
+    // --------------------其他常量--------------------
+    /**
+     * 错误的ID
+     */
+    public static final Long ERROR_ID = -9999L;
+}
--- base-common/src/main/java/cn/imadc/application/base/common/context/ReqCtxConstant.java
+++ base-common/src/main/java/cn/imadc/application/base/common/context/ReqCtxConstant.java
@@ -3,6 +3,7 @@ package cn.imadc.application.base.common.context;
 public class ReqCtxConstant {
     public static final String TOKEN = "token";
     public static final String ID = "id";
+    public static final String NAME = "name";
     public static final String USER = "user";
     public static final String API = "api";
 }
