e33bfb51605d014c1b5d5b3b4c26aa61ab03bd64
dujingsong <1396787304@qq.com>
2022-05-24 17:53:23 +0800

    redis数据解析信息调整

---
+++ base-common/src/main/java/cn/imadc/application/base/common/data/BaseData.java
@@ -0,0 +1,21 @@
+package cn.imadc.application.base.common.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 基础的data
+ * </p>
+ *
+ * @author 杜劲松
+ * @since 2022-05-24
+ */
+@Getter
+@Setter
+public class BaseData implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+}
