96f678a55d58378f213767eacefe80cf71870109
杜劲松 <1396787304@qq.com>
2022-05-25 17:20:37 +0800

    common包

--- base-common/src/main/java/cn/imadc/application/base/common/search/BaseSearchDTO.java
+++ base-common/src/main/java/cn/imadc/application/base/common/search/BaseSearchDTO.java
@@ -1,5 +1,6 @@
 package cn.imadc.application.base.common.search;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,7 +19,9 @@ public class BaseSearchDTO implements Serializable {
 
     private LocalDate startDate;
     private LocalDate endDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime startDateTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime endDateTime;
     private String keywords;
 
