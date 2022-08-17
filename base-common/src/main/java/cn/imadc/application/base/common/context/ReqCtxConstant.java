9714700f1f94a7dc2c16a585e4ee90fa3087701d
杜劲松 <1396787304@qq.com>
2022-08-17 11:08:45 +0800

    增加常量

--- base-common/src/main/java/cn/imadc/application/base/common/context/ReqCtxConstant.java
+++ base-common/src/main/java/cn/imadc/application/base/common/context/ReqCtxConstant.java
@@ -12,6 +12,7 @@ public class ReqCtxConstant {
     public static final String TOKEN = "token";
     public static final String ID = "id";
     public static final String NAME = "name";
+    public static final String USERNAME = "username";
     public static final String USER = "user";
     public static final String API = "api";
 }
