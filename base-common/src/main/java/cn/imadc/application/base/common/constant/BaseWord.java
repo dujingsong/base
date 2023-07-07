2ffbbbd96cb64ddcaee64e3d94cf068ff5c0d62b
杜劲松 <1396787304@qq.com>
2023-07-07 15:47:03 +0800

    常量、工具类

--- base-common/src/main/java/cn/imadc/application/base/common/constant/BaseWord.java
+++ base-common/src/main/java/cn/imadc/application/base/common/constant/BaseWord.java
@@ -14,6 +14,8 @@ public class BaseWord {
     public static final String OP_FAIL = "操作失败";
     public static final String INTERVAL_ERROR = "服务器内部错误...";
     public static final String ILLEGAL_PARAM = "参数错误";
+    public static final String ILLEGAL_PARAM_STYLE = "参数格式错误";
+    public static final String PARSE_HTTP_MESSAGE_FAIL = "解析http请求体失败";
 
 
     // -----------------------------------------会话相关-----------------------------------------
